94965d7792b8ee68a5eabe43b02eb303c3fabf6c
daejin-lee <daejin.lee@nhn.com>
2021-01-20 18:47:25 +0900

    DTO to Entity 변환 구현
    dtoToEntity를 사용하여 DTO를 Guestbook 엔티티로 변환 기능 구현

--- src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -3,12 +3,19 @@ package org.zerock.guestbook.service;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.entity.Guestbook;
 
 @Service
 @Log4j2
 public class GuestbookServiceImpl implements GuestbookService{
     @Override
-    public Long register(GuestbookDTO dto) {
+    public Long register(GuestbookDTO dto){
+        log.info("DTO--------------------------------");
+        log.info(dto);
+
+        Guestbook entity = dtoToEntity(dto);
+        log.info(entity);
+
         return null;
     }
 }
