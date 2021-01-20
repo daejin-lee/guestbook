0951b34780295a3b6c79d01524cd9e8652b30d55
daejin-lee <daejin.lee@nhn.com>
2021-01-20 17:57:19 +0900

    Update Guestbook.java
    Add change Methods

--- src/main/java/org/zerock/guestbook/entity/Guestbook.java
+++ src/main/java/org/zerock/guestbook/entity/Guestbook.java
@@ -20,4 +20,10 @@ public class Guestbook extends BaseEntity{
     private String content;
     @Column(length = 50, nullable = false)
     private String writer;
+    public void changeTitle(String title){
+        this.title = title;
+    }
+    public void changeContent(String content){
+        this.content = content;
+    }
 }
